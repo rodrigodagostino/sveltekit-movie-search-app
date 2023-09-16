8735c2df1ff1471d9362677faffd0016b87d1f71
Rodrigo D’Agostino <rodrigo.dagostino@proton.me>
2023-09-16 17:01:39 -0300

    feat: Create Modal component

---
+++ src/lib/components/Modal.svelte
@@ -0,0 +1,79 @@
+<script lang="ts">
+  import { IconX } from '@tabler/icons-svelte';
+
+  import Button from './Button.svelte';
+
+  let dialogRef: HTMLDialogElement;
+
+  export let title: string | undefined = undefined;
+  export let isModalVisible: boolean;
+
+  $: if (dialogRef && isModalVisible) dialogRef.showModal();
+
+  const closeModalOnBackdropClick = (event: MouseEvent) => {
+    const rect = dialogRef.getBoundingClientRect();
+    const isInDialog =
+      rect.top <= event.clientY &&
+      event.clientY <= rect.top + rect.height &&
+      rect.left <= event.clientX &&
+      event.clientX <= rect.left + rect.width;
+    if (!isInDialog) {
+      dialogRef.close();
+    }
+  };
+</script>
+
+<dialog
+  bind:this={dialogRef}
+  class="modal"
+  on:click={closeModalOnBackdropClick}
+  on:close={() => (isModalVisible = false)}
+>
+  {#if title}
+    <h2 class="modal__title">{title}</h2>
+  {/if}
+  <div class="modal__content">
+    <slot />
+    <Button element="button" variant="secondary-icon" on:click={() => dialogRef.close()}>
+      <IconX size={22} stroke={4} />
+    </Button>
+  </div>
+</dialog>
+
+<style lang="scss">
+  .modal {
+    width: calc(100% - 2rem);
+    max-width: 53.75rem;
+    padding: 1.25rem;
+    border: none;
+    border-radius: 0.75rem;
+    background-color: var(--gray-950);
+    color: inherit;
+
+    &::backdrop {
+      background-color: rgba(0, 0, 0, 0.6);
+    }
+
+    &__title {
+      padding-right: 2.5rem;
+    }
+
+    &__content {
+      margin-top: 1rem;
+    }
+
+    :global(.button) {
+      position: absolute;
+      top: 0.5rem;
+      right: 0.5rem;
+      z-index: 1;
+    }
+  }
+
+  @media screen and (max-width: 46.25em) and (orientation: landscape) {
+    .modal {
+      width: 64%;
+      transform: translate(-50%, -50%);
+    }
+  }
+</style>
