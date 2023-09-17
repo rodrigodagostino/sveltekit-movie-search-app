9fb9cebe0bc1b86142f482b98309c0bf11a034a1
Rodrigo D’Agostino <rodrigo.dagostino@proton.me>
2023-09-16 22:31:42 -0300

    feat: Create Error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,14 @@
+<script>
+  import { page } from '$app/stores';
+</script>
+
+<div class="error">
+  <h1>{$page.status}</h1>
+  <p>{$page?.error?.message}</p>
+</div>
+
+<style lang="scss">
+  .error {
+    text-align: center;
+  }
+</style>
